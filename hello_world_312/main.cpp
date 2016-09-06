1ac66227a583cc0b98e3b81126851ad5534278ad
shadeerafati <srafati@luc.edu>
2016-09-06 17:15:09 -0500

    add hello_world program

---
+++ hello_world_312/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  hello_world_312
+//
+//  Created by Shadee Rafati on 9/1/16.
+//  Copyright © 2016 Shadee Rafati. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "hello world\n";
+    return 0;
+}
